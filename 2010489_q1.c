0f23859d4212c5bc1b97db33c7d1e435fef3892c
shatakshimittal <31719499+shatakshimittal@users.noreply.github.com>
2017-09-07 10:06:40 +0530

    Add files via upload

---
+++ 2010489_q1.c
@@ -0,0 +1,42 @@
+/*....NAME     - Shatakshi Agarwal
+	  SEC      - B
+	  ROLL NO. - 87.....*/
+/*.....LINEAR SEARCH.....*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int arr[10], i, num,t, n, c=0;
+	printf("Enter number of test cases : ");
+	scanf("%d",&t);
+	while(t!=0)
+	{
+		printf("Enter the array size : ");
+		scanf("%d",&n);
+		printf("Enter Array Elements : ");
+		for(i=0; i<n; i++)
+		{
+			scanf("%d",&arr[i]);
+		}
+		printf("Enter the number to be search : ");
+		scanf("%d",&num);
+		for(i=0; i<n; i++)
+		{
+			if(arr[i]==num)
+			{
+				c=1;
+				break;
+			}
+		}
+		if(c==1)
+		{
+			printf("Not Present\nNumber of comparison %d\n\n",i+1);
+		}
+		else
+		{
+			printf("Present\nNumber of comparison %d\n\n",n);
+		}
+		t--;
+    }
+return 0;
+}
